789ee9ff94d6383fd447d2001bf5fbaa38d15e10
misaki_e46 <paul.frankpc@gmail.com>
2024-02-15 19:03:23 -0500

    Initial Commit

---
+++ SwiftWorkshopV2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftWorkshopV2
+//
+//  Created by Paul Frank on 15/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftWorkshopV2/SwiftWorkshopV2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftWorkshopV2App.swift
+//  SwiftWorkshopV2
+//
+//  Created by Paul Frank on 15/02/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftWorkshopV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
